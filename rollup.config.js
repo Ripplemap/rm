37922f9c61032d1d2293355878264eb1fc82f0a2
dann toliver <dann@bentobox.net>
2017-03-03 00:52:00 -0500

    rollup config fixes

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import babel from 'rollup-plugin-babel'
 import includePaths from 'rollup-plugin-includepaths'
 
 var includePathOptions = { include:    {}
-                         , paths:      ['modules']
+                         , paths:      ['js']
                          , external:   []
                          , extensions: ['.js', '.json', '.html']
                          }
 
 export default { entry:     'main.js'
-               , dest:      'bundle.js' // equivalent to --output
+               , dest:      'ripplemap.js' // equivalent to --output
                , format:    'cjs'
                , plugins:   [ includePaths(includePathOptions), json(), babel() ]
                , sourceMap: true
