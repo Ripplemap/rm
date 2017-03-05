1c1c7cf7e1743d24e45034e8c0c0596ac405da91
dann toliver <dann@bentobox.net>
2017-03-05 17:52:04 -0500

    styling changes

--- rollup.config.js
+++ rollup.config.js
@@ -6,10 +6,10 @@ import postcss from 'rollup-plugin-postcss'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
 // post css plugins:
-import simplevars from 'postcss-simple-vars';
-import nested from 'postcss-nested';
-import cssnext from 'postcss-cssnext';
-import cssnano from 'cssnano';
+import simplevars from 'postcss-simple-vars'
+import nested from 'postcss-nested'
+import cssnext from 'postcss-cssnext'
+import cssnano from 'cssnano'
 
 var includePathOptions = { include:    {}
                          , paths:      ['js']
@@ -17,41 +17,26 @@ var includePathOptions = { include:    {}
                          , extensions: ['.js', '.json', '.html']
                          }
 
-export default { entry:     './src/main.js'
+export default { sourceMap: true
+               , format:    'cjs'
+               , entry:     './src/main.js'
                , dest:      './build/bundle.js' // equivalent to --output
-               , format:    'cjs',
-
-                plugins: [
-                  includePaths(includePathOptions),
-                  nodeResolve({jsnext: true}),
-                  postcss({
-                    plugins: [simplevars(), nested(), cssnano(), cssnext({warnForDuplicates: false,})],
-                    extensions: ['.css']
-                  }),
-                  json(),
-                  babel({
-                    babelrc: false,
-                    presets: [ "es2015-rollup"],
-                    plugins: [['transform-react-jsx', { pragma:'h' }], ["transform-class-properties"]],
-                    // exclude: 'node_modules/**',
-                  }),
-                ],
-                  sourceMap: true
-
-
-              //  , plugins:   [ includePaths(includePathOptions),
-              //     nodeResolve()
-              //     , json(), 
-              //     , postcss({
-              //         plugins:    [simplevars(), nested(), cssnano(), cssnext({warnForDuplicates: false,})]
-              //       , extensions: ['.css']
-              //     }),
-              //     babel({
-              //         babelrc:  false
-              //       , presets:  [ "es2015-rollup"]
-              //       , plugins:  [['transform-react-jsx', { pragma:'h' }], ["transform-class-properties"]],
-              //     }),
-              //    ]
-              //  , sourceMap: true
-               // , treeshake : false
+               , plugins: [ includePaths(includePathOptions)
+                          , nodeResolve({jsnext: true})
+                          , json()
+                          , postcss({ extensions: ['.css']
+                                    , plugins: [ simplevars()
+                                               , nested()
+                                               , cssnano()
+                                               , cssnext({warnForDuplicates: false})
+                                               ]
+                                    })
+                          , babel({ babelrc: false
+                                  , presets: [ "es2015-rollup"]
+                                  , plugins: [ ['transform-react-jsx', { pragma:'h' }]
+                                             , ["transform-class-properties"]
+                                             ]
+                                  // exclude: 'node_modules/**',
+                                  })
+                          ]
                }
