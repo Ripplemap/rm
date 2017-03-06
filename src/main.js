1cf6b05a270685280a3011df30e9d226f6520d13
teesloane <tylersloane@gmail.com>
2017-03-05 19:08:16 -0500

    Project organization:
    - moved ripple map logic into src/ripplemap folder
    - moved old files into `old` folder.

--- src/main.js
+++ src/main.js
@@ -2,15 +2,12 @@
  * 
  * The Entry point to all of Ripplemap. (RM)
  * Creates the sidebar for controlling the RM. (built with Preact, see src/Sidebar)
- * Mounts RM to the dom using `createRippleMap()` 
  */
 
 import { h, render, Component } from 'preact';
 import Sidebar from './Sidebar/Sidebar'
-import './main.css'
-import {createRippleMap} from './Ripplemap/Ripplemap';
-import '../js/main'
+import './styles/main.css'
+import './Ripplemap/main'
 
 
 render(<Sidebar />, document.getElementById('sidebar'));
-// createRippleMap()
