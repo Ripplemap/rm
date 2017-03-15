f64636454713f0a5d2531df2db5ae49d167c8219
dann toliver <dann@bentobox.net>
2017-03-14 23:07:03 -0400

    next button!

--- src/Sidebar/Views/Story/index.js
+++ src/Sidebar/Views/Story/index.js
@@ -16,21 +16,24 @@ const Story = () => {
         <form id="login" onSubmit={dom.login} class="Story__form">
           <p>We'd love to hear your stories! Let's start with your contact info:</p>
           <input name="email" id="email" type="text" placeholder="email" class="typeahead Story__input" />
+          <Button type="submit" buttonStyle="next">Next
+            <i class="fa fa-chevron-right pl_1" aria-hidden="true"></i>
+          </Button>
         </form>
       </div>
 
       <div id="storytime" class={state.email ? '' : 'hide'}>
         <h3>Want to add something to the map?</h3>
-        <form id="the-conversation"
-              onSubmit={dom.submit_convo}
-              dangerouslySetInnerHTML={{__html: get_convo_html()}}>
+
+        <form id="the-conversation" onSubmit={dom.submit_convo}>
+          <div dangerouslySetInnerHTML={{__html: get_convo_html()}}></div>
+
+          <Button type="submit" buttonStyle="next">Next
+            <i class="fa fa-chevron-right pl_1" aria-hidden="true"></i>
+          </Button>
         </form>
       </div>
 
-      {/* TODO: hookup OnClick to a function */}
-      <Button type="submit" buttonStyle="next">Next
-        <i class="fa fa-chevron-right pl_1" aria-hidden="true"></i>
-     </Button>
     </div>
   )
 }
